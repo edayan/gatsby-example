37a4e320c070ce351a5b8cedbffac89a00db54de
Saju Paul <saju.christel@blu-pa.com>
2019-07-06 16:54:57 +0530

    css components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
+import "../components/blog.css"
 export default () => {
   return (
     <Layout>
-      <h1>This is our blog page</h1>
+      <h1 className="blog-title">This is our blog page</h1>
     </Layout>
   )
 }
