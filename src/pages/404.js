e1717818fc163e0ffdc4a08d146009bcb3af23a7
Saju Paul <saju.christel@blu-pa.com>
2019-07-06 14:44:23 +0530

    pages

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default Error = () => {
+  return (
+    <div>
+      This is our error page
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default () => (
   <div>
-    <img src="image.jpeg" />
+   <h1>This is our home page</h1>
     Hello world!
   </div>
 )
