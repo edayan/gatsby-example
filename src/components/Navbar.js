4ddf6a7b2c87e8e640911ed265f539436ead5f6b
Saju Paul <saju.christel@blu-pa.com>
2019-07-06 17:51:51 +0530

    sass implementation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styles from './navbar.module.scss'
 const Navbar = () => {
   return (
-    <div>
+    <nav className={styles.navbar}>
+    <h2>company</h2>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/blog/">Blog</Link></li>
         <li><Link to="/products/">Products</Link></li>
       </ul>
-    </div>
+    </nav>
   )
 }
 
