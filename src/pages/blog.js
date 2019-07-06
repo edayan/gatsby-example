276dcc73ead7bed439011eb3572ea1f547584fbb
Saju Paul <saju.christel@blu-pa.com>
2019-07-06 17:11:00 +0530

    Bootstrap

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,6 +8,13 @@ export default () => {
         <h1 className={styles.title}>This is our blog page</h1>
         <h2>This is second heading</h2>
       </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">hello</div>
+          <div className="col-4">hello</div>
+          <div className="col-4">hello</div>
+        </div>
+      </div>
     </Layout>
   )
 }
