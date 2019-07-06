346ebed43f448aeb0005edc545de48184e997a6d
Saju Paul <saju.christel@blu-pa.com>
2019-07-06 14:51:03 +0530

    Link

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from 'gatsby'
 
 export default () => {
   return (
     <div>
       This is our blog page
+      {/* <div> <a href="/">Home page</a></div> */}
+      <Link to="/">Home page</Link>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
+
 
 export default () => (
   <div>
-   <h1>This is our home page</h1>
+    <h1>This is our home page</h1>
     Hello world!
+    <div>
+      {/* {<a href="/blog">blog page</a>} */}
+      <Link to="/blog">blogs</Link>
+    </div>
+    <div>
+      {/* {<a href="https://www.gatsbyjs.org">Gatsby js</a>} */}
+      
+    </div>
   </div>
 )
