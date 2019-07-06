9c2f684b2982afc4cffad9d3a56380f5a147858a
Saju Paul <saju.christel@blu-pa.com>
2019-07-06 15:13:07 +0530

    global css

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react"
+import Layout from "../components/layout"
 export default () => {
   return (
     <Layout>
-      This is our blog page
+      <h1>This is our blog page</h1>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h1 style={{ color: "red", textTransform: "capitalize" }}>
       This is our home page
     </h1>
+    <h1 >Another heading</h1>
     Hello world!
   </Layout>
 )
