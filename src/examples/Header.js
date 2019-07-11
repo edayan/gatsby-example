b3b07505343e398f7cddf5022cdf088a578b632f
Saju Paul <saju.christel@blu-pa.com>
2019-07-11 20:43:29 +0530

    useQuery part :1 restructuring

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 
 const Header = () => {
-  const data = useStaticQuery(graphql`
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery(graphql`
     {
       site {
         siteMetadata {
@@ -18,12 +20,10 @@ const Header = () => {
     }
   `)
 
-  console.log(data)
   return (
     <div>
-      <h1>author: {data.site.siteMetadata.author}</h1>
-      <h1>Title:{data.site.siteMetadata.title}</h1>
-
+      <h1>author: {siteMetadata.author}</h1>
+      <h1>Title:{siteMetadata.title}</h1>
     </div>
   )
 }
