6d2e8867966b798852586ef7e15bada7398a1762
Saju Paul <saju.christel@blu-pa.com>
2019-07-12 19:48:44 +0530

    External variables and aliases

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,29 +1,31 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 
-const Header = () => {
-  const {
-    site: { siteMetadata: info },
-  } = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          description
-          title
-          author
-          data {
-            name
-            age
-          }
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        description
+        siteTitle: title
+        author
+        data {
+          name
+          age
         }
       }
     }
-  `)
+  }
+`
+const Header = () => {
+  const {
+    site: { siteMetadata: info },
+  } = useStaticQuery(getData)
 
+  const title = info.siteTitle
   return (
     <div>
       <h1>author: {info.author}</h1>
-      <h1>Title:{info.title}</h1>
+      <h1>Title:{title}</h1>
     </div>
   )
 }
