225474c32208c71ff99177f8237c2fa478654cab
Saju Paul <saju.christel@blu-pa.com>
2019-07-12 20:29:14 +0530

    pageQuery

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import HeaderHook from '../examples/Header-Hooks'
 import HeaderStatic from '../examples/Header-Static'
+import { graphql } from 'gatsby'
 
+const Examples = props => {
+  const { name, age } = props.data.site.siteMetadata.person
 
-const Examples = () => {
   return (
     <div>
       examples page
-      <HeaderHook/>
-      <HeaderStatic/>
+      <HeaderHook />
+      <HeaderStatic />
+      <div>
+        This is our person
+        <h4>name:{name}</h4>
+        <h4>age:{age}</h4>
+      </div>
     </div>
   )
 }
 
+export const getData = graphql`
+  query {
+    site {
+      siteMetadata {
+        description
+        siteTitle: title
+        author
+        person: data {
+          name
+          age
+        }
+      }
+    }
+  }
+`
 export default Examples
